31bcc598e0ff82ecdf0c3e0fad3bf16a40a0bd2d
Rita Bradley <rita@ritabradley.dev>
2022-10-12 15:28:52 -0700

    remove active class from other panels

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const panels = document.querySelectorAll('.panel')
 
+const removeActiveClass = () => {
+	for (const panel of panels) {
+		if (panel.classList.contains('active')) {
+			return panel.classList.remove('active')
+		}
+	}
+
+}
+
 for (const panel of panels) {
 	panel.addEventListener('click', () => {
-		panel.classList.toggle('active')
+		removeActiveClass()
+		panel.classList.add('active')
 	})
 }
+
